c325ee5c8fb94bcbb53eee770afd2586fafc7147
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-11-05 10:26:44 -0500

    Fix

--- API.js
+++ API.js
@@ -3,7 +3,7 @@ var router_API = express.Router([options]);
 var options;
 
 router_API.get('/route1', function(req, res) {
-    res.send('This worked again'); 
+    res.send('OMG... Google Cloud Kinda Rules'); 
 });
 
 module.exports = router_API;
