bd8b409d8d4d98bec3e86e23c7a2528143fca621
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-11-05 20:03:48 -0500

    Update Test

--- API.js
+++ API.js
@@ -3,7 +3,7 @@ var router_API = express.Router([options]);
 var options;
 
 router_API.get('/route1', function(req, res) {
-    res.send('OMG... Google Cloud Kinda Rules'); 
+    res.send('OMG... Google Cloud Kinda Rules, But Openshift Isn\'t awful.'); 
 });
 
 module.exports = router_API;
